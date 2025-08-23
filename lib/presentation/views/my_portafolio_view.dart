84159f14feec72e173d9ac059b133e704714d383
Martin Vivanco <maviar03@gmail.com>
2025-08-23 19:59:41 -0400

    no puedo creerlo, funciono a la primera. Ahora empezare a ordenarlo por funcionamiento, para que quede mas legible, en un provider aparte.

--- lib/presentation/views/my_portafolio_view.dart
+++ lib/presentation/views/my_portafolio_view.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stocks_app/domain/entities/entities.dart';
+import 'package:stocks_app/presentation/providers/stocks/stocks_repository_provider.dart';
 
 
 
-class MyPortafolioView extends StatelessWidget {
+final FutureProvider<List<Stock>> stockListProvider = 
+FutureProvider((ref) async{
+  final stockRepository = ref.watch(stockRepositoryProvider);
+  return stockRepository.getStock();
+});
+
+class MyPortafolioView extends ConsumerWidget {
   const MyPortafolioView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final stockList = ref.watch(stockListProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Portafolio'),
       ),
 
-      body: const Center(child: Text(':)'),),
+      body: stockList.when(
+        data: (data) => ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            return Text(data[index].symbol);
+          }
+        ), 
+        error: (_, __) => const Text('Algo ha salido mal'), 
+        loading: () => const Center(child: CircularProgressIndicator(strokeWidth: 2,),)
+      )
     );
   }
 }
